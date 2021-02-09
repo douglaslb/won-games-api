79d87caa64f1ca5e6dc5e1ce84c9fe39a833d024
Douglas Lima Barros <dobarros@uolinc.com>
2021-02-09 19:25:41 -0300

    Setting the admin JWT token

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a8e58060c9df60c43d04a5d24d83e0f5'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6d65e29d-fd22-4ed7-9465-081fa56d0e94'
+};
