3c49804e828242618c91e910e093c27eb8093d38
Douglas Lima Barros <dobarros@uolinc.com>
2021-02-17 21:45:09 -0300

    Searching for data already created and avoiding duplication

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -24,6 +24,23 @@ async function getGameInfo(slug) {
   };
 }
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true }),
+    });
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -32,18 +49,8 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products[0]);
+    await create(products[5].publisher, "publisher")
+    await create(products[5].developer, "developer")
 
-    await strapi.services.publisher.create({
-      name: products[0].publisher,
-      slug: slugify(products[0].publisher).toLowerCase(),
-    });
-
-    await strapi.services.developer.create({
-      name: products[0].developer,
-      slug: slugify(products[0].developer).toLowerCase(),
-    });
-
-    // console.log(await getGameInfo(products[0].slug));
   },
 };
